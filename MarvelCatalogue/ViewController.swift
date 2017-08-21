e3da56a307980ced2e794578920446238a471300
Jerry Tromp | The Mobile Company <jerry.tromp@themobilecompany.com>
2017-08-21 18:29:52 +0200

    Initial Commit

---
+++ MarvelCatalogue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarvelCatalogue
+//
+//  Created by Jerry Tromp | The Mobile Company on 21/08/2017.
+//  Copyright © 2017 Jerry Tromp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
