f175465a37bd3429856d5f67f0a323b910386a2a
Jerry Tromp <jerry.tromp@xs4all.nl>
2017-10-04 20:53:03 +0200

    Add CharacterDetailCoordinator

---
+++ MarvelCatalogue/Source/Modules/Character Detail/CharacterDetailCoordinator.swift
@@ -0,0 +1,39 @@
+//
+//  CharacterDetailCoordinator.swift
+//  MarvelCatalogue
+//
+//  Created by Jerry Tromp | The Mobile Company on 04/10/2017.
+//  Copyright © 2017 Jerry Tromp. All rights reserved.
+//
+
+import UIKit
+
+class CharacterDetailCoordinator: Coordinating {
+    var viewController: CharacterDetailViewController?
+    var root: UIViewController
+
+    private let character: MarvelCharacter
+
+    init(root: UIViewController, character: MarvelCharacter) {
+        self.root = root
+        self.character = character
+    }
+
+    func createViewController() -> CharacterDetailViewController {
+        let storyboard = UIStoryboard(name: "CharacterDetail", bundle: nil)
+        guard let view = storyboard.instantiateInitialViewController() as? CharacterDetailViewController else { fatalError() }
+
+        let httpService = HttpService(session: URLSession.shared)
+        let imageGateway = CharacterImageWebGateway(httpService: httpService)
+        let useCase = ShowCharacterDetailUseCase(imageGateway: imageGateway, character: character)
+
+        let presenter = CharacterPresenter(useCase: useCase)
+
+        view.characterPresenter = presenter
+        presenter.view = view
+
+        useCase.presenter = presenter
+
+        return view
+    }
+}
