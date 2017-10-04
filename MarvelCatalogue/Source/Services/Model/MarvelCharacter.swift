117bd395a19072b485fb995c0f965ac611eb1c9c
Jerry Tromp | The Mobile Company <jerry.tromp@themobilecompany.com>
2017-10-04 14:53:58 +0200

    Add id field to MarvelCharacter, and adopt Hashable

--- MarvelCatalogue/Source/Services/Model/MarvelCharacter.swift
+++ MarvelCatalogue/Source/Services/Model/MarvelCharacter.swift
@@ -9,7 +9,18 @@
 import Foundation
 
 struct MarvelCharacter: Codable {
+    let id: Int
     let name: String
     let description: String
     let thumbnail: MarvelImage
+}
+
+extension MarvelCharacter: Hashable {
+    var hashValue: Int {
+        return id
+    }
+
+    static func == (lhs: MarvelCharacter, rhs: MarvelCharacter) -> Bool {
+        return lhs.id == rhs.id
+    }
 }
