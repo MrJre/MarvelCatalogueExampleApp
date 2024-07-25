a4991c25c08a39bce5cc0b90bdf22850e00b03eb
Jerry Tromp <jerry.tromp@nos.nl>
2024-07-25 16:00:53 +0200

    Update prebuild script
    Signed-off-by: Jerry Tromp <jerry.tromp@nos.nl>

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,9 +5,12 @@
 #
 #  Created by Jerry Tromp on 18/10/2023.
 #  Copyright © 2023 Jerry Tromp. All rights reserved.
+
 add_credentials_to_plist() {
     echo 'Adding credentials to plist'
+    env
     /usr/libexec/PlistBuddy -c "clear dict" -c "Add :public string $marvel_public_key" -c "Add :private string $marvel_private_key" '../MarvelCatalogue/Supporting Files/MarvelCredentials.plist'
+    plutil -convert binary1 '../MarvelCatalogue/Supporting Files/MarvelCredentials.plist'
     return 0
 }
 
